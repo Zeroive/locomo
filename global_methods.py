b0e95aa17beb19a237184ff52b3d86a687172a19
zeroive <905175111@qq.com>
2026-07-03 20:23:52 +0800

    update: 修改自定义模型服务的端口地址
    将自定义模型服务的基础URL端口从24073更新为24075

--- global_methods.py
+++ global_methods.py
@@ -11,7 +11,7 @@ from anthropic import Anthropic
 os.environ['NO_PROXY'] = 'localhost,127.0.0.1'
 
 # 自定义模型服务配置
-CUSTOM_MODEL_BASE_URL = "http://7.216.57.92:24073/v1"
+CUSTOM_MODEL_BASE_URL = "http://7.216.57.92:24075/v1"
 CUSTOM_MODEL_NAME = "Qwen3-32B"
 CUSTOM_EMBEDDING_BASE_URL = "http://7.216.57.92:24067/v1"
 CUSTOM_EMBEDDING_API_KEY = "token-abc123"
